e1ad8c5d6c54626b0e350b2ae6ffe3e821fcaa68
vverac <viverace@gmail.com>
2023-07-05 21:13:03 -0400

    creacion de templates,conexion a postgreqsl, superuser

--- src/app_tlv/views.py
+++ src/app_tlv/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+  template_name='app_tlv/landing.html'
+
+def bienvenido(request):
+  return render(request, 'app_tlv/bienvenido.html') 
